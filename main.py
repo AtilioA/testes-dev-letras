0279dcb2e74d7457544b837140d7d372e5832557
AtilioA <27153776+AtilioA@users.noreply.github.com>
2021-10-09 00:11:37 -0300

    Checa feat por árvore trie, modulariza tratamento de strings
    Remove checagem ingênua de feat

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 
 import trie.trienode as trie
 
+
 class EnumScore(Enum):
     """Enum que armazena os diferentes valores de pontuação."""
 
@@ -14,38 +15,29 @@ class EnumScore(Enum):
 
 import utils.string_utils as str_utils
 
+
 class Musica:
     """Classe que abstrai uma música do banco de músicas."""
 
     def __init__(self, titulo: list[str], tituloOriginal: str) -> None:
-        # Título da música
+        # Título da música, tratado e divido em uma lista de strings
         self.titulo: list[str] = titulo
-        # Índice no vetor original de músicas
+        # Título original da música
         self.tituloOriginal: str = tituloOriginal
         # Pontuação da música para a busca atual
         self.score: int = 0
         # Booleano representando caso a música tenha feat
         self.temFeat: bool = None
-        self.verifica_feat()  # Atualiza self.temFeat
-
-    def verifica_feat(self) -> None:
-        """Determina se strings de entrada possuem 'feat' e retorna pontuação de acordo com lógica estabelecida pelo desafio."""
-
-        for palavra in self.get_titulo():
-            if palavra == "feat":
-                return self.set_feat(True)
-        # Caso nenhuma palavra da música seja 'feat'
-        return self.set_feat(False)
 
     def pontua_feat(self, stringBusca: str) -> None:
-        """Determina se string de entrada possui 'feat' e retorna pontuação de acordo com lógica estabelecida pelo desafio para a música em questão."""
+        """Checa se string possui feat e retorna pontuação de acordo com lógica estabelecida pelo desafio para a string buscada."""
 
         if self.tem_feat():
             if stringBusca == "feat":
                 # Usuário está procurando feat, não desconte ponto
                 return self.adiciona_score(EnumScore["WANT_FEAT"].value)
             else:
-                # Usuário não está procurando feat (mas música possui 'feat'), desconte ponto
+                # Usuário não está procurando feat mas música possui 'feat', desconte ponto
                 return self.adiciona_score(EnumScore["DONT_WANT_FEAT"].value)
         else:
             # Não há feat, não desconte ponto
@@ -76,43 +68,47 @@ class Musica:
         return self.temFeat
 
 
-def busca_musicas(stringBusca: str, stringsMusicas: list[str]) -> list[Musica]:
-    # Trata string de busca para ignorar capitalização e acentos
-    stringBusca = str_utils.trata_string(str(stringBusca).strip())
-
-    # Divide string de busca para comparar palavra por palavra
-    stringBuscaDividida: list[str] = stringBusca.split()
-    # Lista de objetos Musica para armazenar objetos com o score devidamente computado
-    musicas: list[Musica] = []
-
-    # Trata strings do banco de músicas da mesma forma que a da busca
+def trata_e_divide_strings_banco(stringsMusicas: list[str]):
+    """Trata e divide strings da lista de músicas do banco em uma lista para cada palavra."""
+    # Trata strings do banco de músicas da mesma forma que a string de busca
     stringsBancoTratadas = map(str_utils.trata_string, stringsMusicas)
     # Divide títulos das músicas por espaços para comparar palavra por palavra
     stringsBancoDivididas: list[list[str]] = list(
         map(str_utils.divide_string_por_espaco, stringsBancoTratadas)
     )
 
-    # Percorre todas as músicas do banco de músicas
-    for indice, titulo in enumerate(stringsBancoDivididas):
-        # Cria objeto Musica para cada música, salvando o título original do vetor de strings do banco
-        # Aqui deve-se haver cuidado para não realizar cópia e utilizar memória descenessariamente
-        # Como strings são imutáveis em Python e não pretendemos alterar esta string, o seguinte custa pouca memória:
-        objMusica = Musica(titulo, stringsMusicas[indice])
+    return stringsBancoDivididas
 
+
+def cria_objs_musica(musicas: list[list[str]]):
+    """Cria um objeto Musica para cada lista de string da lista dada como entrada"""
+    # Cria objeto Musica para cada música, salvando o título original do vetor de strings do banco
+    # Aqui deve-se haver cuidado para não realizar cópia e utilizar memória descenessariamente
+    # Como strings são imutáveis em Python e não pretendemos alterar esta string, o seguinte custa pouca memória:
+
+    objsMusicas = []
+
+    for indice, titulo in enumerate(musicas):
+        objMusica = Musica(titulo, str_utils.stringsBanco[indice])
+        objsMusicas.append(objMusica)
+
+    return objsMusicas
+
+
+def busca_musicas(stringsBusca: list[str], musicas: list[Musica]) -> list[Musica]:
+    # Percorre todas as músicas do banco de músicas
+    for i, musica in enumerate(musicas):
         # Compara cada palavra de busca
-        for palavraBusca in stringBuscaDividida:
+        for palavraBusca in stringsBusca:
             # Com cada palavra do objeto Musica recém criado
-            for palavraMusica in objMusica.get_titulo():
+            for palavraMusica in musica.get_titulo():
                 # Adiciona pontos de acordo com as correspondências
-                objMusica.adiciona_score(
+                musicas[i].adiciona_score(
                     str_utils.compara_strings_ingenuo(palavraBusca, palavraMusica)
                 )
 
             # Retira pontos caso apenas a música possua a palavra 'feat'
-            objMusica.pontua_feat(palavraBusca)
-
-        # Adiciona o objeto Musica, agora com pontuação correta, a um vetor
-        musicas.append(objMusica)
+            musicas[i].pontua_feat(palavraBusca)
 
     return musicas
 
@@ -123,14 +119,32 @@ if __name__ == "__main__":
 
     # Lê string de busca da entrada
     stringEntrada = input("# Digite sua busca: ")
-    print("#")
+    # Trata string de busca para ignorar capitalização e acentos
+    stringEntrada = str_utils.trata_string(stringEntrada).strip()
+    # Divide string de busca para comparar palavra por palavra
+    stringEntradaDividida: list[str] = stringEntrada.split()
 
-    musicas = busca_musicas(stringEntrada, str_utils.stringsBanco)
-    print("# Resultados:")
+    # Lista com títulos das músicas separados por espaço (i.e., dividos em lista de strings)
+    stringsBanco: list[list[str]] = trata_e_divide_strings_banco(str_utils.stringsBanco)
+
+    # Lista de objetos Musica para armazenar objetos com o score devidamente computado
+    musicas: list[Musica] = cria_objs_musica(stringsBanco)
+
+    # Monta árvore trie para buscar por músicas com palavra 'feat'
+    raizTrie = trie.TrieNode()
+    raizTrie.popula_trie_musicas(musicas)
+    musicasComFeat = raizTrie.busca("feat")
+    for musica in musicasComFeat:
+        musica.set_feat(True)
+
+    # Faz comparação ingênua
+    musicas = busca_musicas(stringEntradaDividida, musicas)
 
     # Ordena músicas por score (decrescente, desempate não importa)
     musicas.sort(key=lambda musica: musica.score, reverse=True)
 
+    print("#")
+    print("# Resultados:")
     # Percorre imprimindo até TOP_N_MUSICAS, neste caso 10
     for musica in musicas[:TOP_N_MUSICAS]:
         print(f"# {musica.score} pontos, {musica.tituloOriginal}")
